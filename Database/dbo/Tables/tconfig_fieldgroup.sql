8dfe7166e7694d5f54f3915ae73213de7b422546
COMDATA\stefano.laterra <stefano.laterra@dp2000.it>
2018-10-24 16:19:28 +0200

    Updated formula configuration view. Allowed null operator in fieldgroup table.
    Formula config view now returns multiple rules in a nested "rules" node.

--- Database/dbo/Tables/tconfig_fieldgroup.sql
+++ Database/dbo/Tables/tconfig_fieldgroup.sql
@@ -2,10 +2,12 @@
     [tf_codint]            INT           IDENTITY (1, 1) NOT NULL,
     [tf_name]              VARCHAR (100) NOT NULL,
     [tf_prevGroupOperator] VARCHAR (1)   NULL,
-    [tf_operator]          VARCHAR (1)   NOT NULL,
+    [tf_operator]          VARCHAR (1)   NULL,
     [tf_rule_id]           INT           NOT NULL,
     [tf_prevGroup]         INT           NULL,
     CONSTRAINT [PK_TCONFIG_FIELDGROUP] PRIMARY KEY CLUSTERED ([tf_codint] ASC),
     CONSTRAINT [fk_tconfig_fieldgroup_tconfig_rules] FOREIGN KEY ([tf_rule_id]) REFERENCES [dbo].[tconfig_rules] ([tr_codint]) ON UPDATE CASCADE
 );
+
+
 
--- Database/dbo/Views/vconfig_formula_configuration.sql
+++ Database/dbo/Views/vconfig_formula_configuration.sql
@@ -12,34 +12,40 @@ AS SELECT
 		FOR XML PATH('Tipology'), TYPE, ELEMENTS
 	  ) FOR XML PATH('Tipologies'), TYPE) as Tipologies
  ,(SELECT
-      tcf_codint AS [@Id]	  
+      tf.tcf_codint AS [@Id]	  
      ,tf.tcf_name AS [@Name]
 	 
      ,tf.tcf_description AS [Description]
-     ,tr.tr_codint AS [Rule/@Id]
-     ,tr.tr_name AS [Rule/@Name]
-     ,tr.tr_description AS [Rule/Description]
-     ,tr.tr_prevrule AS [Rule/PreviousRule]
-     ,[Rule/FieldGroups] = (SELECT
-          tf1.tf_codint AS [@Id]
-         ,tf1.tf_name AS [@Name]
-         ,tf1.tf_prevgroupoperator AS [PrevGroupOperator]
-         ,tf1.tf_operator AS [Operator]
-         ,tf1.tf_prevgroup AS [PrevGroup]
-         ,[Fields] = (SELECT
-              tf2.tf_name AS [@Name]
-             ,tf2.tf_codint AS [@Id]
-			 , tff_codint AS [FieldId]
-            FROM tconfig_fields tf2
-            INNER JOIN tconfig_fieldgroup_fields tff
-              ON tf1.tf_codint = tff.tff_fieldgroup_id
-            WHERE tff.tff_field_id = tf2.tf_codint
-            FOR XML PATH ('Field'), TYPE, ELEMENTS)
-        FROM tconfig_fieldgroup tf1
-        WHERE tr.tr_codint = tf1.tf_rule_id
-        FOR XML PATH ('FieldGroup'), TYPE, ELEMENTS)
+
+      , [Rules] = 
+      (   SELECT 
+
+          tr.tr_codint AS [@Id]
+         ,tr.tr_name AS [@Name]
+         ,tr.tr_description AS [Description]
+         ,tr.tr_prevrule AS [PreviousRule]
+         ,[FieldGroups] = (SELECT
+              tf1.tf_codint AS [@Id]
+             ,tf1.tf_name AS [@Name]
+             ,tf1.tf_prevgroupoperator AS [PrevGroupOperator]
+             ,tf1.tf_operator AS [Operator]
+             ,tf1.tf_prevgroup AS [PrevGroup]
+             ,[Fields] = (SELECT
+                  tf2.tf_name AS [@Name]
+                 ,tf2.tf_codint AS [@Id]
+    			      , tff_codint AS [FieldId]
+                FROM tconfig_fields tf2
+                INNER JOIN tconfig_fieldgroup_fields tff
+                  ON tf1.tf_codint = tff.tff_fieldgroup_id
+                WHERE tff.tff_field_id = tf2.tf_codint
+                FOR XML PATH ('Field'), TYPE, ELEMENTS
+                )
+            FROM tconfig_fieldgroup tf1
+            WHERE tr.tr_codint = tf1.tf_rule_id
+            FOR XML PATH ('FieldGroup'), TYPE, ELEMENTS)
+       FROM tconfig_rules tr WHERE tr.tr_formula = tf.tcf_codint
+       FOR XML PATH('Rule'), TYPE
+    )   
     FOR XML PATH ('Formula'), TYPE)
   AS Definition
 FROM tconfig_formulas tf
-INNER JOIN tconfig_rules tr
-  ON tf.tcf_codint = tr.tr_formula
